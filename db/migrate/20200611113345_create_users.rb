3e229182d96dd7537bbf69395f3b556d78d7f365
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-23 21:23:04 +0900

    パスワード再設定用カラム追加

--- db/migrate/20200611113345_create_users.rb
+++ db/migrate/20200611113345_create_users.rb
@@ -4,10 +4,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :encrypted_password
       t.string :access_token
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
 
       t.timestamps
     end
     add_index :users, :name, unique: true
     add_index :users, :access_token, unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,13 @@ ActiveRecord::Schema.define(version: 2020_06_11_113345) do
     t.string "name"
     t.string "encrypted_password"
     t.string "access_token"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["access_token"], name: "index_users_on_access_token", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
