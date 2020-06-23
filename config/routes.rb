b06bbd5223743eaa889d17d98330bdf1bee4d6c1
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-23 21:28:49 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   resources :memos
+  resources :passwords, only: %i[create edit update]
   post 'login/login'
+
+  # LetterOpenerWeb::Engineをマウントする
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
